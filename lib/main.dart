dd9da0969cf9cc25752210386b9b2e1a69666d1d
Eason Lin <lin00212@algonquinlive.com>
2024-12-02 20:28:47 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          textTheme: Theme.of(context).textTheme.copyWith(
+              bodyLarge: TextStyle(fontSize: 20, color: Colors.pinkAccent))),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  List<Widget> _screen = [HomePage(), DataPage(), ContactPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screen[_currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.data_usage), label: 'Data'),
+          NavigationDestination(
+              icon: Icon(Icons.contact_mail), label: 'Contact'),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/background.png'),
+                      fit: BoxFit.cover)),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.secondary,
+              child: Center(
+                child: Transform.rotate(
+                  angle: 0.15,
+                  child: Text(
+                    "Hello World",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DataPage extends StatefulWidget {
+  const DataPage({super.key});
+
+  @override
+  State<DataPage> createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  late Future<List<Product>> _data;
+
+  @override
+  void initState() {
+    //TODO: implement initState
+    super.initState();
+    _data = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Data Page')),
+      body: FutureBuilder<List<Product>>(
+        future: _data,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: snapshot.data![index].thumbnail != null &&
+                              snapshot.data![index].thumbnail.isNotEmpty
+                          ? Image.network(snapshot.data![index].thumbnail)
+                          : Icon(Icons.image_not_supported),
+                      title: Text(snapshot.data![index].title),
+                      subtitle: Text(snapshot.data![index].description),
+                    ),
+                  );
+                });
+          } else {
+            return Text('Failed to load data');
+          }
+        },
+      ),
+    );
+  }
+
+  Future<List<Product>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://dummyjson.com/products/'));
+    print(response.body);
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body)['products'];
+      return jsonData
+          .map((data) => Product(
+              id: data['id'],
+              title: data['title'],
+              description: data['description'],
+              thumbnail: data['thumbnail']))
+          .toList();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _name;
+  String? _email;
+  String? _message;
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // Handle the form submission, such as sending to a server
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Form Submitted Successfully!')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Contact Us")),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.account_box),
+                  labelText: 'Name',
+                  hintText: 'Enter your name',
+                ),
+                keyboardType: TextInputType.name,
+                textInputAction: TextInputAction.next,
+                style: const TextStyle(color: Colors.green),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Name is required!';
+                  }
+                },
+                onSaved: (value) {
+                  _name = value;
+                },
+              ),
+              const SizedBox(height: 18),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.alternate_email),
+                  labelText: 'Email',
+                  hintText: 'Enter your email address',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                style: const TextStyle(color: Colors.green),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email is required!';
+                  }
+                },
+                onSaved: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(height: 18),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.message_rounded),
+                  labelText: 'Message',
+                  hintText: 'Enter your message',
+                ),
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                style: const TextStyle(color: Colors.green),
+                maxLines: 3,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Message is required!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _message = value;
+                },
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _submitForm,
+                  child: const Text('Submit Now'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final String thumbnail;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.thumbnail,
+  });
+}
